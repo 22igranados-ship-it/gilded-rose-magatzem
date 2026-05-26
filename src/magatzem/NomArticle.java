3fc3b1e8fda30f2cdae069109da03c6152b7a4db
usuario <usuario@DESKTOP-597M9HI>
2026-05-26 19:54:36 +0200

    refactor: extreure constants de noms a NomArticle

---
+++ src/magatzem/NomArticle.java
@@ -0,0 +1,7 @@
+package magatzem;
+
+public class NomArticle {
+    public static final String FORMATGE_GIDURAT = "Formatge Gidurat";
+    public static final String MARTELL_DE_THOR  = "Martell de Thor (Llegendari)";
+    public static final String ENTRADES_CONCERT = "Entrades per al Concert del Trobador";
+}
